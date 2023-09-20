ba0fc5dbb16998fd830dbca90db3b4d068a2298c
Cesar993 <ce.serrano@duocuc.cl.com>
2023-09-20 12:49:27 -0300

    act

--- script.js
+++ script.js
@@ -8,41 +8,30 @@ function borrar() {
     cookie.remove();
 }
 
-var far = document.querySelector(".red");
-var far1 = document.querySelector(".red1");
-var far2 = document.querySelector(".red2");
-var far3 = document.querySelector(".red3");
 
-var fBlue = document.querySelector(".blue");
-var fBlue1 = document.querySelector(".blue1");
-var fBlue2 = document.querySelector(".blue2");
-var fBlue3 = document.querySelector(".blue3");
+function faren(val) {
+    return Math.round(5/9 * (val-32));
+}
+function cel(val) {
+    return Math.round(9/5 * val +32);
+}
 
 
 function chan(element) {
 
-    if (element.value == "°F") {
-        far.innerText = "167°";
-        far1.innerText = "176";
-        far2.innerText = "156.2";
-        far3.innerText = "172.4";
-        
-        fBlue.innerText = "149";
-        fBlue1.innerText = "150.8";
-        fBlue2.innerText = "141.8";
-        fBlue3.innerText = "158";
+    for (let i = 1; i < 9; i++) {
+        var temp = document.querySelector(".valor"+i);
+        var valorEntero = parseInt(temp.innerText);
 
-    }
-    else if (element.value == "°C") {
-        far.innerText = "75";
-        far1.innerText = "80";
-        far2.innerText = "69";
-        far3.innerText = "78";
-
-        fBlue.innerText = "65";
-        fBlue1.innerText = "66";
-        fBlue2.innerText = "61";
-        fBlue3.innerText = "70";
+         if (element.value == "°F") {
+            temp.innerText = cel(valorEntero) ;
+        }
+        else if (element.value == "°C") {
+            temp.innerText = faren(valorEntero);
+        } 
     }
 
 }
+
+
+
